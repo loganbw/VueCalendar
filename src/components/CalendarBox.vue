30fafddaff9320d0105b3a4354d5b526e5d65a53
Logan W <loganw@IMI-Mac1.local>
2022-03-08 14:51:26 -0500

    updating calendar to show today date

--- src/components/CalendarBox.vue
+++ src/components/CalendarBox.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="calendarBox" >
-    <table id="calendarGrid" name="calendar" :load=" checkForStartDay(index)">
+  <div class="calendarBox">
+    <table id="calendarGrid" name="calendar">
       <tr v-for="(weekday, index) in DaysOfTheWeek" class="weekday">
         {{
           weekday
@@ -10,19 +10,19 @@
         v-for="(box, index) in numberOfBoxes"
         :class="isActive ? 'boxActive' : 'box'"
         :id="index"
+        :key="index"
         ref="test"
         
       >
         {{
           displayDate(index)
         }}
-       
       </tr>
     </table>
   </div>
 </template>
 <script>
-let t = 1;
+let t = 0;
 export default {
   props: {
     firstDayTest: Date,
@@ -34,7 +34,7 @@ export default {
     "DaysOfTheWeek",
     "firstDayOfMonth",
     "daystoMonth",
-    "day"
+    "day",
   ],
   data() {
     return {
@@ -54,10 +54,17 @@ export default {
   },
   mounted() {
     this.loadDayArray();
+    this.checkRef;
   },
   methods: {
+    test1(index){
+      console.log(index)
+      if(index + 1 == this.today)
+         return this.isActive = true;
+      else return this.active = false
+    },
     checkForStartDay(isday) {
-       if (isday === this.today) return (this.isActive = true);
+      if (isday === this.today) return (this.isActive = true);
       else {
         return (this.isActive = false);
       }
@@ -66,7 +73,7 @@ export default {
       for (let index = 0; index <= this.daysInMonthTest; index++) {
         this.days.push(index);
       }
-      console.log(this.days.length)
+      console.log(this.days.length);
     },
     updateDay() {
       return (this.day = this.day + 1);
@@ -75,18 +82,29 @@ export default {
       console.log(item);
     },
     displayDate(index) {
-       //this.checkForStartDay(index)
-      if (index >= this.firstDayTest.getDay()) {
+      //this.checkForStartDay(index)
 
-        if (t < this.days.length) {
-          return t++;
+      if (index >= this.firstDayTest.getDay()) {
+        if (t < this.days.length - 1) {
+          t++;
+          return t;
         }
+      }
+    },
+  },
+  computed: {
+    checkRef() {
+      console.log(this.$refs.test[1].id);
+      for (let index = 0; index < this.daysInMonthTest; index++) {
+        if (this.$refs.test[index].id === this.day.toString())
+          this.isActive = true;
+        else this.isActive = false;
       }
     },
   },
   watch: {
     daysInMonthTest() {
-      t = 1;
+      t = 0;
       this.resetDay = false;
       this.days = [];
       this.loadDayArray();
